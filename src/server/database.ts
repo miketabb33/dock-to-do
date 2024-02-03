5acb3896a3e06e1ca6269b90bb0820ff474f82c9
miketabb33 <miketabb33@gmail.com>
2024-02-03 08:05:49 -0500

    Use pool and inject props into db query

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,14 +1,14 @@
 import { uuid } from '../uuid'
 import { ToDoDto } from '../dto/ToDoDto'
-import { Client } from 'pg'
+import { Pool } from 'pg'
 
-const client = new Client({
+const pool = new Pool({
   database: 'postgres',
 })
 
 const connectToDatabase = async () => {
   try {
-    await client.connect()
+    await pool.connect()
     console.log('Postgres Successfully Connected')
   } catch (err) {
     console.error(`MY SQL: error connecting: ${err}`)
@@ -16,7 +16,7 @@ const connectToDatabase = async () => {
 }
 
 const runDatabaseSchema = () => {
-  client.query(`
+  pool.query(`
     CREATE TABLE IF NOT EXISTS list (
       id VARCHAR(50) NOT NULL,
       message VARCHAR(255) NOT NULL,
@@ -32,7 +32,7 @@ export const setupDatabase = () => {
 
 export class ToDoDatabase {
   getToDos = async (): Promise<ToDoDto[]> => {
-    const results = await client.query('SELECT * FROM list;')
+    const results = await pool.query('SELECT * FROM list;')
     const items: ToDoDto[] = results.rows
     const sortedItems = items.sort((a, b) => {
       const aNum = new Date(a.created_on).getTime()
@@ -45,17 +45,16 @@ export class ToDoDatabase {
   createToDo = async (message: string) => {
     const now = new Date()
     const q = 'INSERT INTO list(id, message, created_on) VALUES ($1, $2, $3)'
-    await client.query(q, [uuid(), message, now])
+    await pool.query(q, [uuid(), message, now])
   }
 
   deleteToDo = async (id: string) => {
-    await client.query(`DELETE FROM list WHERE id='${id}';`)
+    const q = 'DELETE FROM list WHERE id=$1;'
+    await pool.query(q, [id])
   }
 
   editToDo = async (id: string, message: string) => {
-    await client.query(`
-      UPDATE list
-      SET message = '${message}'
-      WHERE id = '${id}';`)
+    const q = 'UPDATE list SET message=$1 WHERE id=$2;'
+    await pool.query(q, [message, id])
   }
 }
